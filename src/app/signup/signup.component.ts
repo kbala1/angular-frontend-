dc9f1d319e6f889aedac5357d0c5a554d6ded9eb
kiran <kiranrakesh@gmail.com>
2021-05-10 15:29:43 -0500

    Add login and logout forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,21 @@
+import {UserService} from 'src/app/services/user/user.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public userName: string | undefined;
+  public emailAddress: string | undefined;
+  public password: string | undefined;
+  registerUser(): void {
+    const newUser = {userName: this.userName, emailAddress: this.emailAddress, password: this.password};
+    this.userService.registerUser(newUser);
   }
-
+  constructor(private userService: UserService) {
+  }
+  ngOnInit(): void {
+    console.log("signed up");
+  }
 }
